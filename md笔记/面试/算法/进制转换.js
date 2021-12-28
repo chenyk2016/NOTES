905f67c9b55015574193fc51502c681769772621
鹿鸣 <chenyakun@innotechx.com>
2021-12-28 12:23:07 +0800

    1

--- md笔记/面试/算法/进制转换.js
+++ md笔记/面试/算法/进制转换.js
@@ -2,10 +2,10 @@
 /**
  *
  * @param {*} val 10进制数字
- * @param {*} type 进制
+ * @param {*} radix 进制
  */
-function transform(val, type) {
-  if(type < 2) {
+function transform(val, radix) {
+  if(radix < 2) {
     throw ('转换的进制不符合要求')
   }
   let res = []
@@ -13,9 +13,9 @@ function transform(val, type) {
   let pos = 0
   let resV = val
   while(resV !== 0) {
-    const posV = Math.floor(resV / Math.pow(type, pos) )
+    const posV = Math.floor(resV / Math.pow(radix, pos) )
     if(posV === 0) {
-      const preMax = Math.pow(type, pos - 1)
+      const preMax = Math.pow(radix, pos - 1)
       res[pos - 1] = Math.floor(resV / preMax )
       resV = resV % preMax
       pos = 0
@@ -27,17 +27,16 @@ function transform(val, type) {
   for(let i = res.length - 1; i >= 0 ; i--) {
     let v = res[i] || 0
     if(v > 10) {
-      v = String.fromCharCode(v - 10 + 96)
+      v = String.fromCharCode(v - 10 + 97)
     }
     resStr += v
   };
 
-  // console.log(res);
-
   return resStr
 }
 
-console.log( transform(32, 16) );
-console.log( transform(15, 8) );
-console.log( transform(1000, 10) );
-console.log( transform(10, 2) );
+console.log( transform(31, 16) ); // 1f
+console.log( transform(31, 8) ); // 37
+console.log( transform(31, 32) ); // v
+console.log( transform(31, 10) ); // 31
+console.log( transform(31, 2) ); // 11111
